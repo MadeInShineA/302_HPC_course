0089c622705fb39586631db434e0fa2ac0a98cbd
MadeInShineA <olivier.amacker@netplus.ch>
2025-10-29 14:22:00 +0100

    Finished ex4

--- mpi/easy/exo4/main.cpp
+++ mpi/easy/exo4/main.cpp
@@ -1,10 +1,12 @@
+#include <chrono>
+#include <cstdlib> // for rand()
 #include <iostream>
 #include <mpi.h>
 
 using namespace std;
+using namespace chrono;
 
 int main(int argc, char **argv) {
-
   MPI_Init(&argc, &argv);
 
   int world_size;
@@ -13,10 +15,12 @@ int main(int argc, char **argv) {
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  int NUMBERS_PER_ARRAY = 100;
-  int num_arrays[world_size][NUMBERS_PER_ARRAY];
+  const int NUMBERS_PER_ARRAY = 1'000'000;
 
+  // Only rank 0 allocates the full array (dynamically to avoid stack overflow)
+  int (*num_arrays)[NUMBERS_PER_ARRAY] = nullptr;
   if (rank == 0) {
+    num_arrays = new int[world_size][NUMBERS_PER_ARRAY];
     for (int i = 0; i < world_size; i++) {
       for (int j = 0; j < NUMBERS_PER_ARRAY; j++) {
         num_arrays[i][j] = rand() % 100;
@@ -24,15 +28,34 @@ int main(int argc, char **argv) {
     }
   }
 
-  MPI_Barrier(MPI_COMM_WORLD);
-
   int num_array[NUMBERS_PER_ARRAY];
-  MPI_Scatter(num_arrays, NUMBERS_PER_ARRAY, MPI_INT, num_array,
-              NUMBERS_PER_ARRAY, MPI_INT, 0, MPI_COMM_WORLD);
+  int local_sum = 0;
+  int global_sum = 0;
 
-  if (rank != 0) {
-    cout << "Process - " << rank << " " << num_array << endl;
+  auto start = high_resolution_clock::now();
+
+  // Scatter data from rank 0 to all ranks
+  MPI_Scatter((rank == 0) ? &(num_arrays[0][0]) : nullptr, NUMBERS_PER_ARRAY,
+              MPI_INT, num_array, NUMBERS_PER_ARRAY, MPI_INT, 0,
+              MPI_COMM_WORLD);
+
+  // EVERY rank (including rank 0) computes its local sum
+  for (int i = 0; i < NUMBERS_PER_ARRAY; i++) {
+    local_sum += num_array[i];
+  }
+
+  // Reduce all local sums to rank 0
+  MPI_Reduce(&local_sum, &global_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+  auto end = high_resolution_clock::now();
+  auto duration = duration_cast<milliseconds>(end - start);
+
+  if (rank == 0) {
+    cout << "Sum of the array: " << global_sum << "\n"
+         << "Duration: " << duration.count() << " milliseconds" << endl;
+    delete[] num_arrays; // Clean up
   }
 
   MPI_Finalize();
+  return 0;
 }
